0be39667c52ea8169f6269588b37ad9aca8fd437
TreusK <flutri@protonmail.com>
2023-10-22 21:54:23 -0300

    modified ran num depending on sets

--- script.js
+++ script.js
@@ -23,16 +23,34 @@ function handleSubmit(e) {
   	return;
   }
 
-  //Grab any number from the array of available sets
-  let randomCheckNum = checkedArr[Math.floor(Math.random() * checkedArr.length)]
-  
   //Create random X length number
-  let lengthLimit = stringLength.value * 6;
+  let randomNum = makeRandomOfLength(stringLength);
+
+  //Modify random number to fit chosen sets (if all sets arent chosen)
+  if(checkedArr.length != 10) {
+  	let randomNumArr = randomNum.split('');
+  	for(let i=0; i<= randomNumArr.length; i+=2) {
+  		randomNumArr[i] = getRandomFromArr(checkedArr);
+  	}
+  	randomNum = randomNumArr.join('');
+  }
+  
+  //Show num on screen
+  generatedNum.innerText = randomNum
+
+}
+
+
+//Helpers
+function getRandomFromArr(arr) {
+	return arr[Math.floor(Math.random() * arr.length)]
+}
+
+function makeRandomOfLength(strLnght) {
+	let lengthLimit = strLnght.value * 6;
   let randomNum = '';
   for(let i=0; i<lengthLimit; i++)  {
   	randomNum += Math.floor(Math.random() * 6);
   }
-
-  //Show num on screen
-  generatedNum.innerText = randomNum
+  return randomNum;
 }
