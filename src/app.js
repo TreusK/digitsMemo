d2d195bcf8b7d6f405b176f600d4295d9f8695ed
TreusK <flutri@protonmail.com>
2023-10-23 20:50:00 -0300

    timer working, need to validate result still

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 let submitBtn = document.querySelector('.submitBtn');
 let memoForm = document.querySelector('.memoForm');
 let stringLength = document.querySelector('#stringLength');
-let timer = document.querySelector('#timer');
+let timerForm = document.querySelector('#timerForm');
 let checkBoxes = document.querySelectorAll('.form-check-input');
 let generatedNum = document.querySelector('.generatedNum');
+let generatedNumContainer = document.querySelector('.generatedNumContainer');
+let resultFormContainer = document.querySelector('.resultFormContainer');
 
+let timer = document.querySelector('#timer');
+let timerContainer = document.querySelector('.timerContainer')
+let timerInterval;
 
 memoForm.addEventListener('submit', handleSubmit);
+
 
 function handleSubmit(e) {
  	e.preventDefault();
@@ -36,9 +42,13 @@ function handleSubmit(e) {
   }
   
   //Show num on screen
-  generatedNum.innerText = randomNum
-
+  generatedNum.innerText = randomNum;
+  createTimer(timerForm.value);
+  showElem(generatedNumContainer);
+  showElem(timerContainer); 
+  hideElem(resultFormContainer);
 }
+
 
 
 //Helpers
@@ -54,3 +64,44 @@ function makeRandomOfLength(strLnght) {
   }
   return randomNum;
 }
+
+
+function createTimer(time) {
+	clearInterval(timerInterval);
+
+  let minute = Math.floor(time / 60);
+	let second = time % 60;
+
+  timerInterval = setInterval(function () {
+    timer.innerHTML =
+      (minute < 10 ? '0' + minute : minute) +
+      ':' +
+      (second < 10 ? '0' + second : second);
+
+    if (minute != 0 && second == 0) {
+      minute--;
+      second = 60;
+    }
+
+    if (minute == 0 && second == 0) {
+  		hideElem(timerContainer);
+  		hideElem(generatedNumContainer);
+  		showElem(resultFormContainer);
+  		clearInterval(timerInterval);
+  	}
+
+  	second--;
+
+  }, 1000);
+
+};
+
+function hideElem(elem) {
+	elem.classList.add('d-none');
+}
+
+function showElem(elem) {
+	elem.classList.remove('d-none');
+}
+
+
